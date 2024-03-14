94e0b0a72348204cb22f3e79cf4f0208fbecc892
adrisala20 <adriana_15_tuya@hotmail.com>
2024-03-14 15:25:25 +0100

    add variables to first snack palindroma

---
+++ script.js
@@ -0,0 +1,33 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+//chiedere all'utente di inserire una parola palindroma
+let userWord = prompt('inserisci una parola palindroma');
+console.log(userWord);
+
+function getWordUser(wordPalindroma){
+    newWordSplit = 
+    for()
+}
+
+
+
+
+
+
+/*
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+
+
+//funzione numeri random
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
